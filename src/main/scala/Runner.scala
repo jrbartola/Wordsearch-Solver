84d5e0b4d1213c0b4303b0630988df0614db4f50
Jesse Bartola <jrbartola@gmail.com>
2016-12-24 17:32:30 -0500

    Runner modifications

--- src/main/scala/Runner.scala
+++ src/main/scala/Runner.scala
@@ -5,13 +5,19 @@ import WordSearch._
 object Solver extends App {
 
   val s = new Scanner(System.in);
-  println("Enter the number of lines proceeded by the wordsearch itself")
+  println("Enter the number of lines your word search has:")
   val n = s.nextInt
+  println("Enter your word search: ")
 
+  // Clear the buffer
+  s.nextLine
   val wsmat = 0.until(n).map(x => s.nextLine.replace(" ", "").map(c => c).toArray).toArray
-  val ws = new WordSearch(wsmat, (n, wsmat(0).length))
+  val ws = WordSearch(wsmat, (n, wsmat(0).length))
+
+  println("Enter a list of words to find: ")
 
   val wlist = 0.to(26).map(x => s.nextLine).toList
+  
   println("Let's solve your Word Search...")
   print(ws.find(wlist))
 
