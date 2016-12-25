661c4f50dcbd6f620b19f0fc24339545d7bcf89b
Jesse Bartola <jrbartola@gmail.com>
2016-12-25 15:05:53 -0500

    Added diagonal coordinate enumeration functionality to ImplicitHelpers

--- src/main/scala/ImplicitTuple.scala
+++ src/main/scala/ImplicitTuple.scala
@@ -5,7 +5,24 @@ object ImplicitHelpers {
 
     // An implementation of the .to construct implicitly defined for int tuples
     def to(tup2: (Int, Int)): List[(Int, Int)] = {
-      tup._1.to(tup2._1).flatMap(x => tup._2.to(tup2._2).map(y => (x, y))).toList
+
+      // We need to split this up if we are dealing with diagonals
+      if (tup._1 != tup2._1 && tup._2 != tup2._2) {
+
+        if (tup._2 > tup2._2) {
+          // Build up a collection of cordinates on the respective diagonal
+          tup._1.to(tup2._1).foldLeft(List[(Int, Int)]()) { (li: List[(Int, Int)], n: Int) => 
+            (tup._1 + n, tup._2 - n)
+          }
+        } else {
+          0.to(tup2._1-tup._1).foldLeft(List[(Int, Int)]()) { (li: List[(Int, Int)], n: Int) => 
+            (tup._1 + n, tup._2 + n)
+          }
+        }
+      } else {
+        tup._1.to(tup2._1).flatMap(x => tup._2.to(tup2._2).map(y => (x, y))).toList
+      }
+      
     }
 
   }
